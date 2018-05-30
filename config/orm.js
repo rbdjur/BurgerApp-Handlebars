db7dff897436fd879a94c6ca752fc2d0ce657aec
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-05-29 18:52:06 -0700

    organized files and created all directories needed

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,24 @@
 var connection = ("/connection.js");
 console.log("Here");
 console.log(connection);
+
+var orm = {
+    select: function(whatToSelect, tableInput) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+selectWhere: function(tableInput, colToSearch, valOfCal) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    console.log(queryString);
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCal], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+}
+
+module.exports = orm;
