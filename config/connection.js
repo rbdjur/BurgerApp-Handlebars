a9fdb23fcb7ecfa382c54ecbda1405bc3d687060
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-06-07 18:42:29 -0700

    heroku edits to server.js and connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,21 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db",
+    });
+};
+
 
 connection.connect(function(err) {
     if (err) {
